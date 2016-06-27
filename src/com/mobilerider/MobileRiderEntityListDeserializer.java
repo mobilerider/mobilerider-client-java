59db4528b865517b58b789330fea933f0bb86d6b
Nelson R <nelson@eifferent.com>
2016-06-26 21:46:32 -0600

    .

--- src/com/mobilerider/MobileRiderEntityListDeserializer.java
+++ src/com/mobilerider/MobileRiderEntityListDeserializer.java
@@ -2,12 +2,10 @@ package com.mobilerider;
 
 import com.google.gson.*;
 import com.google.gson.annotations.SerializedName;
-import com.google.gson.internal.ObjectConstructor;
 
 import java.lang.reflect.Array;
 import java.lang.reflect.Type;
 import java.util.Arrays;
-import java.util.List;
 
 class MobileRiderEntityListDeserializer<TEntity> implements JsonDeserializer<Page<TEntity>>
 {
